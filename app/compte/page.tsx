15f699924dd8188199daf5a25d709bedc0400361
Guillaume Birrien <guillaumebirrien@gmail.com>
2025-04-06 22:32:20 +0200

    Ajout des cards sur l'espace de compte

--- app/compte/page.tsx
+++ app/compte/page.tsx
@@ -4,6 +4,15 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabase";
 import { toast } from "sonner";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/src/components/ui/card"
+import { BookmarkCheck, SquareUserRound } from "lucide-react";
 
 export default function Compte() {
     const router = useRouter();
@@ -39,13 +48,27 @@ export default function Compte() {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen space-y-4">
+        <div className="flex flex-col items-center justify-start min-h-screen pt-32 space-y-8 px-4">
             <h1 className="text-2xl font-bold">Espace Mon Compte</h1>
             {userEmail ? (
                 <p className="text-gray-700">Connecté en tant que : <strong>{userEmail}</strong></p>
             ) : (
                 <p className="text-gray-500">Chargement...</p>
             )}
+            <div className="grid grid-cols-2 md:grid-cols-2 gap-6 w-full max-w-2xl">
+                <Card className="cursor-pointer hover:shadow-lg transition p-6 text-center">
+                    <CardContent className="flex flex-col items-center space-y-2 pt-4">
+                        <SquareUserRound size={36} />
+                        <span className="text-lg font-semibold">Gérer mon compte</span>
+                    </CardContent>
+                </Card>
+                <Card className="cursor-pointer hover:shadow-lg transition p-6 text-center">
+                    <CardContent className="flex flex-col items-center space-y-2 pt-4">
+                        <BookmarkCheck size={36} />
+                        <span className="text-lg font-semibold">Articles enregistrés</span>
+                    </CardContent>
+                </Card>
+            </div>
             <button onClick={handleLogout} className="px-6 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition">
                 Se déconnecter
             </button>
